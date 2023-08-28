3a8812c1ec6310e2ac4beb5032f0b450dcc646d3
Jacob Marks <jamarks13@gmail.com>
2023-08-28 14:18:46 -0700

    updates + BLIPv2

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Visual Question Answering Plugin
 
-This plugin is a Python plugin that allows you to answer visual questions about images in your dataset!
+This plugin is a Python plugin that allows you to answer visual questions about
+images in your dataset!
 
 It demonstrates how to do the following:
 
@@ -10,24 +11,45 @@ It demonstrates how to do the following:
 
 ### Supported Models
 
-This version of the plugin supports the default Vision Language Transformer (ViLT) model used in the [Visual Question Answering pipeline](https://huggingface.co/tasks/visual-question-answering) in the Hugging Face transformers library.
+This version of the plugin supports the default Vision Language Transformer
+(ViLT) model used in the [Visual Question Answering pipeline](https://huggingface.co/tasks/visual-question-answering)
+in the Hugging Face transformers library, as well as Salesforce's
+[BLIPv2](https://replicate.com/andreasjansson/blip-2) via
+[Replicate](https://replicate.com/).
 
 Feel free to fork this plugin and add support for other models!
 
 ## Installation
 
-```shell
-fiftyone plugins download https://github.com/jacobmarks/vqa-plugin
-```
+### Pre-requisites
 
-Install the Hugging Face transformers library:
+1. If you plan to use it, install the Hugging Face transformers library:
 
 ```shell
 pip install transformers
+```
+
+2. If you plan to use it, install the Replicate library:
+
+```shell
+pip install replicate
+```
+
+And add your Replicate API key to your environment:
+
+```shell
+export REPLICATE_API_TOKEN=<your-api-token>
+```
+
+### Install the plugin
+
+```shell
+fiftyone plugins download https://github.com/jacobmarks/vqa-plugin
 ```
 
 ## Operators
 
 ### `answer_visual_question`
 
-- Generates an image from a text prompt and adds it to the dataset
+- Applies the selected visual question answering model to the selected sample in
+  your dataset and outputs the answer.
--- __init__.py
+++ __init__.py
@@ -5,20 +5,94 @@
 |
 """
 
-
+from importlib.util import find_spec
+import os
 from PIL import Image
-from transformers import pipeline
-
 
 import fiftyone as fo
+import fiftyone.core.utils as fou
 import fiftyone.operators as foo
 from fiftyone.operators import types
+
+
+transformers = fou.lazy_import("transformers")
+replicate = fou.lazy_import("replicate")
+
+
+def allows_replicate_models():
+    """Returns whether the current environment allows replicate models."""
+    return (
+        find_spec("replicate") is not None
+        and "REPLICATE_API_TOKEN" in os.environ
+    )
+
+
+def allows_hf_models():
+    """
+    Returns whether the current environment allows hugging face transformer
+    models.
+    """
+    return find_spec("transformers") is not None
 
 
 def get_filepath(sample):
     return (
         sample.local_path if hasattr(sample, "local_path") else sample.filepath
     )
+
+
+class VQAModel:
+    """Wrapper around a VQA model."""
+
+    def __init__(self):
+        pass
+
+    # def _get_image(self, sample):
+    #     filepath = get_filepath(sample)
+    #     image = Image.open(filepath)
+    #     return image
+
+    def __call__(self, sample, question):
+        pass
+
+
+class ViLTVQAModel(VQAModel):
+    """Wrapper around a Hugging Face ViLT VQA model."""
+
+    def __call__(self, sample, question):
+        filepath = get_filepath(sample)
+        image = Image.open(filepath)
+        from transformers import pipeline
+
+        vqa_pipeline = pipeline("visual-question-answering")
+        response = vqa_pipeline(image, question, top_k=1)
+        return response[0]["answer"]
+
+
+class BLIP2VQAModel(VQAModel):
+    """Wrapper around Replicate BLIP2 VQA model."""
+
+    def __call__(self, sample, question):
+        filepath = get_filepath(sample)
+        response = replicate.run(
+            "andreasjansson/blip-2:4b32258c42e9efd4288bb9910bc532a69727f9acd26aa08e175713a0a857a608",
+            input={"image": open(filepath, "rb"), "question": question},
+        )
+        return response
+
+
+def _get_vqa_model(model_name):
+    if model_name == "vilt":
+        return ViLTVQAModel()
+    if model_name == "blip2":
+        return BLIP2VQAModel()
+
+    raise ValueError(f"Model {model_name} not found.")
+
+
+def run_vqa(sample, question, model_name):
+    model = _get_vqa_model(model_name)
+    return model(sample, question)
 
 
 class VQA(foo.Operator):
@@ -34,6 +108,29 @@ class VQA(foo.Operator):
         inputs = types.Object()
         form_view = types.View(
             label="VQA", description="Ask a question about the selected image!"
+        )
+
+        blip_flag = allows_replicate_models()
+        vilt_flag = allows_hf_models()
+        if not blip_flag and not vilt_flag:
+            inputs.message(
+                "message",
+                label="No models available. Please set up your environment variables.",
+            )
+            return types.Property(inputs)
+
+        radio_choices = types.RadioGroup()
+        if blip_flag:
+            radio_choices.add_choice("blip2", label="BLIP2")
+        if vilt_flag:
+            radio_choices.add_choice("vilt", label="ViLT")
+
+        inputs.enum(
+            "model_name",
+            radio_choices.values(),
+            default=radio_choices.choices[0].value,
+            label="Model",
+            view=radio_choices,
         )
 
         num_selected = len(ctx.selected)
@@ -58,14 +155,11 @@ class VQA(foo.Operator):
 
     def execute(self, ctx):
         sample = ctx.dataset[ctx.selected[0]]
-        filepath = get_filepath(sample)
-        image = Image.open(filepath)
-
         question = ctx.params.get("question", "None provided")
-        vqa_pipeline = pipeline("visual-question-answering")
-        response = vqa_pipeline(image, question, top_k=1)
+        model_name = ctx.params.get("model_name", "blip2")
+        answer = run_vqa(sample, question, model_name)
 
-        return {"question": question, "answer": response[0]["answer"]}
+        return {"question": question, "answer": answer}
 
     def resolve_output(self, ctx):
         outputs = types.Object()
